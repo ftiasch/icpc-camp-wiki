b73c3bea964e21859127e0e7a0c66c61284de4ef
Xiaoxu Guo <ftiasch0@gmail.com>
2018-05-02 09:49:54 +0800

    added nightfall/go.py

---
+++ nightfall/go.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+
+with open('../order', 'r') as of:
+    for ff in of.readlines():
+        f = ff.rstrip()
+        print(f)
+        sp.run(['git', 'add', f])
+        sp.run(['git', 'commit', '-m', f])
+        input()
