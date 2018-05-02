45fac3bc68feab3be7b5b0e16c0a6103e075c029
Xiaoxu Guo <ftiasch0@gmail.com>
2018-05-03 01:20:14 +0800

    added nightfall/git_add_and_commit.sh

--- nightfall/git_add_and_commit.sh
+++ nightfall/git_add_and_commit.sh
@@ -1,6 +1,9 @@
-#!/bin/bash -x
+!/bin/bash -x
+=============
+
 set -o errexit
 
-#pandoc -i "$1" -t markdown -o "$1"
-git add "$1"
-git commit -uno -m "added $1"
+pandoc -i "\$1" -t markdown -o "\$1"
+====================================
+
+git add "\$1" git commit -uno -m "added \$1"
